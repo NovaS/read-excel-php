39ccdcabc6c4c87ee43ad2c1479873bffe512acb
Nova Saputra <nova.saputra@gmail.com>
2018-09-17 01:51:28 +0700

    update read file

--- read.php
+++ read.php
@@ -1,35 +1,26 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
 date_default_timezone_set("Asia/Jakarta");
+require __DIR__ . '/vendor/autoload.php';
+require __DIR__.'/module/dependencies.php';
 
 use Port\Excel\ExcelReader;
-use Module\Dao\Database;
 use Module\Util\Timer;
 
-$pdo = Database::getPdo();
-$file = new \SplFileObject('voucher.xlsx');
+$serviceid = getenv('VOUCHER_SERVICE_ID'); 
+$filename = getenv('VOUCHER_FILE_EXCEL');
+$file = new \SplFileObject($filename);
 $reader = new ExcelReader($file);
-
-if(isset($reader) && isset($pdo)) {
-    $reader->setColumnHeaders(['Coupons']);
+$dao = $container['daoDinoVoucher'];
+if(isset($reader) && isset($dao)) {
+    $reader->setColumnHeaders(['VOUCHER']);
     $reader->setHeaderRowNumber(0);
     $timer = new Timer(1);
     $starttime = $timer->get();
     $elapsedtime = $starttime;
     for($r=1; $r<=$reader->count(); $r++) {
         $row = $reader->getRow($r);
-        $code = $row['Coupons'];
-        try {
-            $sql = "INSERT INTO dino_voucher (voucher_serviceid,voucher_msisdn,voucher_voucher,voucher_prosestime,voucher_expiretime,voucher_usage) ";
-            $sql.= "VALUES ('2029','',:code,now(),str_to_date('2017-12-31 23:59:59','%Y-%m-%d %H:%i:%s'),0)";
-            $query = $pdo->prepare($sql);
-			$query->bindParam(':code',$code);
-			$query->execute();
-			$result = $pdo->lastInsertId();
-			$query = null;
-        } catch (PDOException $e) {
-            print $e->getMessage().PHP_EOL;
-        }
+        $code = trim($row['VOUCHER']);
+        $count = $dao->insert($serviceid,$code);
         if($r%1000===0) { // TODO: comment this section for faster performance
             $processtime = $timer->get() - $elapsedtime;
             print "Process $r takes $processtime".PHP_EOL;
